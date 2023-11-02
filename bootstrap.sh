b77b8757a9fe79a16b38b6fd31411fa64eb9c3eb
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-11-02 10:04:52 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -82,6 +82,13 @@ sudo chown ${USER}:${USER} ${HOME}/.kube/config
 kubectl apply -f ${CURRENT_DIR}/sa.yaml
 kubectl apply -f ${CURRENT_DIR}/clusterrolebinding.yaml
 
+# download nerdctl zip file
+cd ${HOME}
+wget https://github.com/containerd/nerdctl/releases/download/v1.6.2/nerdctl-full-1.6.2-linux-amd64.tar.gz
+
+# install nerdctl
+tar Cxzvvf /usr/local nerdctl-full-1.6.2-linux-amd64.tar.gz
+
 # install gpu-operator
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
   && helm repo update
